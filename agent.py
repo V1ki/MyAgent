3fddaed784e29dd3a83b142ac624ffa4da635a70
V1ki <kunwanjian@qq.com>
2025-02-12 10:25:58 +0800

    Refactor agent.py to add test functions for text and visual understanding models

--- agent.py
+++ agent.py
@@ -10,23 +10,36 @@ from model_api import get_response
 load_dotenv()  # 从 .env 文件中加载环境变量
 # 功能结束: 加载环境变量
 
-# 功能开始: 主程序入口
-if __name__ == "__main__":
+
+# 测试开始: 文本模型
+def test_text_model():
     user_input = "What is the meaning of life?"
+    
+    model_spec = "dashscope:qwen-max-latest"
+    model_spec = "zhipu:glm-4-flash"
     model_spec = "gemini:gemini-2.0-flash-thinking-exp-01-21"
+    model_spec = "gemini:gemini-2.0-flash"
+    
     result = get_response(prompt=user_input, model_spec=model_spec)  # 调用 model_api 模块中的请求方法
     print("Agent Answer:", result)
-    
-    print("====================================")
-    # 功能开始: VLM 视觉理解 模型 测试
-    image_path = "resouce/lianpu.jpeg"
+# 测试结束: 文本模型
+
+
+# 测试开始: 视觉理解 模型 
+def test_vlm_model():
+    user_input = "extract the main content of the image"
     model_spec = "dashscope:qwen2.5-vl-72b-instruct"
-    user_input = "Extract the text from the image."
+    image_path = "resouce/lianpu.jpeg"
     result = get_response(prompt=user_input, model_spec=model_spec, image_path=image_path)  # 调用 model_api 模块中的请求方法
     print("Agent Answer:", result)
-    # 功能结束: VLM 视觉理解 模型 测试
+# 测试结束: 视觉理解 模型
+
+# 功能开始: 主程序入口
+if __name__ == "__main__":
     
-    
+    # test_text_model()
+    print("====================================")
+    # test_vlm_model()
     
     
 # 功能结束: 主程序入口
